9358135a7321b911c33a88a3229dc198d859473e
Pete Peterson <peter.peterson.iv@gmail.com>
2016-12-06 11:29:51 -0700

    updated keymap

--- keymap.c
+++ keymap.c
@@ -43,6 +43,7 @@ extern keymap_config_t keymap_config;
 #define SPCRGT LCTL(KC_RGHT)
 #define SPCLFT LCTL(KC_LEFT)
 #define ENTSFT SFT_T(KC_ENT)
+#define CTLESC CTL_T(KC_ESC)
 #define VOLUP  S(LALT(KC_VOLU))
 #define VOLDN  S(LALT(KC_VOLD))
 #define POWER S(LCTL(KC_POWER))
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
   {KC_TABM, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTSFT },
   {FUNC, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPFN,  KC_SPFN,   RAISE,   KC_RGUI, KC_RALT, KC_RCTL, FUNC}
 },
@@ -93,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_GAM3R] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTSFT },
   {GAMEUP,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC, KC_SPC,   RAISE,   KC_RGUI, KC_RALT, KC_RCTL,  FUNC}
 },
@@ -131,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, KC_7,  KC_8,  KC_9, KC_PLUS,  VOLUP  },
   {XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, KC_4,  KC_5,  KC_6, KC_MINS,  VOLDN  },
   {KC_CAPS, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_0,    KC_1,  KC_2,  KC_3, KC_ASTR,  KC_MUTE},
-  {_______, _______, _______, POWER , KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, HYPER , _______, _______, _______}
+  {_______, KC_LSFT, _______, POWER , KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT, HYPER , _______, KC_RSFT, _______}
 },
 
 
@@ -147,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_ACL2, KC_ACL1, KC_ACL0,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX, XXXXXXX },
   {_______, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX },
   {XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX },
-  {XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX }
+  {XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_BTN1, _______, _______, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX }
 }
 
 
